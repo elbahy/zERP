4d3c7f481c806f8e2d92b9ed0d4163280b76846b
Mohamed Elbahy <vb.supporter@gmail.com>
2024-07-29 02:02:29 +0300

    main, login Form

--- zERP/PL/FormLogin.cs
+++ zERP/PL/FormLogin.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace zERP
+namespace zERP.PL
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
         }
